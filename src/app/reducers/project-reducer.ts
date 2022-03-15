4d554275a807493426de4ece8e4f5672c5c195d1
Zartab Nakhwa <zartab@codewithz.com>
2022-03-15 11:46:11 +0530

    Redux: Added Reducer Function

--- src/app/reducers/project-reducer.ts
+++ src/app/reducers/project-reducer.ts
@@ -1,7 +1,31 @@
 import { Project } from "../projects/project";
+import { Action } from './../actions/index';
+import * as projectAction from '../actions/projects-action';
+
 
 export interface ProjectReducerState {
-    loading: boolean,
-    loaded: boolean,
-    projects: Project[]
+    loading: boolean;
+    loaded: boolean;
+    projects: Project[];
+}
+
+
+const initialState: ProjectReducerState = {
+    loading: false,
+    loaded: false,
+    projects: []
+}
+
+export function userReducer(state = initialState, action: Action) {
+    switch (action.type) {
+        case projectAction.PROJECTS_LIST_REQUESTED: {
+            return { ...state, loading: true }
+        }
+        case projectAction.PROJECTS_LIST_SUCCESS: {
+            const updatedProjects = state.projects.concat(action.payload.data);
+            return { ...state, loading: false, loaded: true, projects: updatedProjects }
+        }
+        default:
+            return state;
+    }
 }
