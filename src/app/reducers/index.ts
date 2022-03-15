9f985a7eeb2e579981099abe1fa88aa43293edbd
Zartab Nakhwa <zartab@codewithz.com>
2022-03-15 12:16:58 +0530

    Redux: Added Selectors to RootReducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,12 +1,20 @@
 
-import { ActionReducerMap } from '@ngrx/store';
+import { ActionReducerMap, createSelector } from '@ngrx/store';
 import { projectReducer, ProjectReducerState } from './project-reducer';
+import * as fromProject from './project-reducer'
 
 export interface RootReducerState {
-    projects: ProjectReducerState,
+    projects: fromProject.ProjectReducerState,
 
 };
 
 export const rootReducer: ActionReducerMap<RootReducerState> = {
-    projects: projectReducer
+    projects: fromProject.projectReducer
 };
+
+export const getProjectState = (state: RootReducerState) => state.projects;
+
+//Actual Selectors that components will subscribe
+export const getProjectLoaded = createSelector(getProjectState, fromProject.getLoaded);
+export const getProjectLoading = createSelector(getProjectState, fromProject.getLoading);
+export const getProjects = createSelector(getProjectState, fromProject.getProjects);
